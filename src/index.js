50558b1618fe02f5fa81a7d4298cd70566366103
Matt Brennan <mattyb1000@gmail.com>
2016-02-01 14:50:30 +0000

    tree grafting update

--- src/index.js
+++ src/index.js
@@ -1,18 +1,19 @@
 const {loadConfig, install, recalculateMetadata, lsFromTree} = require('./npm');
 const hash = require('object-hash');
 const pick = require('lodash.pick');
+const filter = require('lodash.filter');
 const transform = require('lodash.transform');
 const renameKeys = require('@quarterto/rename-keys');
+const transitiveDeps = require('@quarterto/transitive-dependencies');
 const npmlog = require('npmlog');
 
-export const getIdealTree = () => loadConfig({
+export const getIdealTree = (where = null, packages = []) => loadConfig({
 	'dry-run': true,
-	'no-optional': true,
+	'optional': false,
 	'progress': false,
-	'no-shrinkwrap': true
+	'no-shrinkwrap': true,
 })
-	.then(() => install())
-	.then(tree => recalculateMetadata(tree, npmlog))
+	.then(() => install(where, packages))
 	.then(tree => lsFromTree('', tree, [], true)); // args, in order: package root (not important), tree, deps to list (empty array means all), silent mode (i.e. just return the tree obj)
 
 export function sanitiseDep(dependency) {
@@ -24,14 +25,16 @@ export function sanitiseDep(dependency) {
 }
 
 export function depTreeToGraph(deps, from = 'root', edges = [], refs = {}) {
+	var rootDeps = {};
 	for(let k in deps) {
 		var sanitised = sanitiseDep(deps[k]);
 		var hashed = hash(sanitised);
 		edges.push([from, hashed]);
 		refs[hashed] = sanitised;
+		rootDeps[sanitised.name] = hashed;
 		depTreeToGraph(deps[k].dependencies, hashed, edges, refs);
 	}
-	return {edges, refs};
+	return {edges, refs, rootDeps};
 }
 
 export const depGraphToTree = ({edges, refs}, node = 'root') => transform(edges.filter(edge => edge[0] === node), (tree, edge) => {
@@ -39,3 +42,14 @@ export const depGraphToTree = ({edges, refs}, node = 'root') => transform(edges.
 	dep.dependencies = depGraphToTree({edges, refs}, edge[1]);
 	tree[dep.name] = dep;
 }, {});
+
+export function graftTree(root, {edges: oldEdges, refs: oldRefs, rootDeps: oldRootDeps}, {edges: newEdges, refs: newRefs, rootDeps: newRootDeps}) {
+	var rootHash = oldRootDeps[root];
+	var deps = [rootHash, ...transitiveDeps(oldEdges, rootHash)];
+
+	return {
+		edges: oldEdges.filter(edge => !deps.some(dep => edge[0] === dep || edge[1] === dep)).concat(newEdges),
+		refs: Object.assign(pick(oldRefs, (pkg, phash) => !~deps.indexOf(phash)), newRefs),
+		rootDeps: Object.assign(oldRootDeps, newRootDeps)
+	};
+}
--- src/npm.js
+++ src/npm.js
@@ -12,6 +12,6 @@ const {recalculateMetadata: _recalc} = require('npm/lib/install/deps');
 const promisify = require('@quarterto/promisify');
 
 export const loadConfig = promisify(_loadConfig);
-export const install = promisify((cb) => _install([], (err, installed, tree) => cb(err, tree)));
+export const install = promisify((where, packages, cb) => _install(where, packages, (err, installed, tree) => cb(err, tree)));
 export const recalculateMetadata = promisify(_recalc);
 export const lsFromTree = promisify(_lsFromTree);
