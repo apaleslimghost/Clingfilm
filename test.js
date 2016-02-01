37453558c330d03df90b76e314cd839c77ae7cff
Matt Brennan <mattyb1000@gmail.com>
2016-02-01 16:01:46 +0000

    more tests

--- test.js
+++ test.js
@@ -7,10 +7,41 @@ const fakePackage = (name, version) => ({name, version, id: `${name}@${version}`
 module.exports = {
 	depTreeToGraph: {
 		'basic tree': {
+			before() {
+				let foo = this.foo = fakePackage('foo', '1.0.0');
+				this.hashed = hash(foo);
+				this.graph = depTreeToGraph({foo});
+			},
+			
 			'should have an edge from root to package'() {
-				let foo = fakePackage('foo', '1.0.0');
-				let hashed = hash(foo);
-				expect(depTreeToGraph({foo}).edges).to.deep.equal([['root', hashed]]);
+				expect(this.graph.edges).to.deep.equal([['root', this.hashed]]);
+			},
+
+			'should have a ref to the package'() {
+				expect(this.graph.refs).to.deep.equal({[this.hashed]: this.foo});
+			},
+
+			'should have the package as a root dep'() {
+				expect(this.graph.rootDeps).to.deep.equal({'foo': this.hashed});
+			}
+		},
+
+		'simple deps': {
+			before() {
+				let foo = this.foo = fakePackage('foo', '1.0.0');
+				let bar = this.foo = fakePackage('bar', '1.0.0');
+				this.hashedFoo = hash(foo);
+				this.hashedBar = hash(bar);
+				foo.dependencies = {bar};
+				this.graph = depTreeToGraph({foo});
+			},
+		
+			'should have an edge from package to dep'() {
+				expect(this.graph.edges).to.include.something.that.deep.equals([this.hashedFoo, this.hashedBar]);
+			},
+
+			'should not have the dep as a root dep'() {
+				expect(this.graph.rootDeps).not.to.have.property('bar');
 			}
 		}
 	}
